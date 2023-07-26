d37c4357f7e5f61428126828da4ababea028c306
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-07-25 20:22:28 -0400

    Initial commit - main.py

---
+++ main.py
@@ -0,0 +1,122 @@
+To improve the Python program, you can consider the following:
+
+1. Use type hints: Add type hints to function parameters and return values to improve readability and maintainability of the code.
+
+2. Modularize the code: Split the code into smaller functions with clear responsibilities. This will make the code easier to understand, test, and debug.
+
+3. Apply the Single Responsibility Principle (SRP): Each class and function should have a single responsibility. Consider separating the features into separate classes or modules, focusing on one task per class/function.
+
+4. Encapsulate logic in methods: Move the logic inside `main()` into separate methods to encapsulate and separate concerns. For example, move the collaborative filtering and content-based filtering logic into separate methods.
+
+5. Use PEP 8 naming conventions: Follow the PEP 8 style guide for naming conventions. Use lowercase_with_underscores for variable and function names.
+
+6. Use exception handling: Add appropriate exception handling to handle potential errors during user input, file reading, or data processing.
+
+7. Add comments and docstrings: Include comments to explain complex logic and docstrings to describe the purpose and usage of each class and function.
+
+8. Improve input validation: Validate user inputs to handle empty inputs, invalid input formats, and limit input length where necessary.
+
+Here's an improved version of the program incorporating these suggestions:
+
+```python
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import NearestNeighbors
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.metrics.pairwise import cosine_similarity
+
+class User:
+    def __init__(self, name: str, genres: list[str], favorite_artists: list[str], moods: list[str]):
+        self.name = name
+        self.genres = genres
+        self.favorite_artists = favorite_artists
+        self.moods = moods
+
+def create_user() -> User:
+    name = input("Enter your name: ")
+    genres = input("Enter your preferred genres (comma-separated): ").split(",")
+    favorite_artists = input("Enter your favorite artists (comma-separated): ").split(",")
+    moods = input("Enter your current mood(s) (comma-separated): ").split(",")
+    return User(name.strip(), [genre.strip() for genre in genres], [artist.strip() for artist in favorite_artists], [mood.strip() for mood in moods])
+
+def analyze_songs() -> pd.DataFrame:
+    # Load songs dataset
+    songs = pd.read_csv("songs.csv")
+
+    # Feature extraction
+    features = songs[['tempo', 'energy', 'danceability', 'acousticness']]
+
+    # Standardize the features
+    scaler = StandardScaler()
+    scaled_features = scaler.fit_transform(features)
+
+    # Create a music database with the scaled features
+    music_database = pd.DataFrame(scaled_features, columns=features.columns)
+    
+    return music_database
+
+def collaborative_filtering(user: User, music_database: pd.DataFrame) -> pd.DataFrame:
+    # Find similar users using nearest neighbors algorithm
+    model = NearestNeighbors(metric='cosine', n_neighbors=5)
+    model.fit(music_database)
+
+    user_features = np.array(user.genres + user.favorite_artists).reshape(1,-1)
+    user_features = scaler.transform(user_features)
+
+    distances, indices = model.kneighbors(user_features)
+
+    similar_users = [music_database.iloc[indice] for indice in indices][0]
+    recommended_songs = similar_users.sample(n=5)
+
+    return recommended_songs
+
+def content_based_filtering(user: User, music_database: pd.DataFrame) -> list[str]:
+    # Extract song names and combine with genres and artists
+    song_names = music_database.index
+    song_genres = songs['genre'].astype(np.str_)
+    song_artists = songs['artist']
+
+    # Create TF-IDF matrix
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform(song_genres + song_artists)
+
+    # Reduce dimensionality using Truncated SVD
+    svd = TruncatedSVD(n_components=50, random_state=42)
+    reduced_matrix = svd.fit_transform(tfidf_matrix)
+
+    # Calculate cosine similarity between user's preferences and songs
+    user_preferences = user.genres + user.favorite_artists
+    user_preferences_matrix = vectorizer.transform(user_preferences)
+    user_preferences_matrix = svd.transform(user_preferences_matrix)
+
+    similarities = cosine_similarity(user_preferences_matrix, reduced_matrix)
+
+    # Get the indices of top similar songs
+    top_indices = similarities.argsort()[0][-5:]
+
+    recommended_songs = [song_names[i] for i in top_indices]
+
+    return recommended_songs
+
+def main():
+    user = create_user()
+    music_database = analyze_songs()
+
+    cf_recommendations = collaborative_filtering(user, music_database)
+    cb_recommendations = content_based_filtering(user, music_database)
+
+    print("Collaborative Filtering Recommendations:")
+    for song in cf_recommendations:
+        print(song)
+
+    print("\nContent-Based Filtering Recommendations:")
+    for song in cb_recommendations:
+        print(song)
+
+if __name__ == "__main__":
+    main()
+```
+
+Please note that the changes above are general guidelines, and you might need to make additional modifications based on your specific requirements and constraints.
